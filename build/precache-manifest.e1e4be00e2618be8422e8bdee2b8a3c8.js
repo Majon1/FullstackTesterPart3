eee1d20d66bf98f976dd4d85924b4951098f34d8
Majon1 <mathilda.nynas@gmail.com>
2020-10-12 21:58:19 +0300

    build added

---
+++ build/precache-manifest.e1e4be00e2618be8422e8bdee2b8a3c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44add86d6637f71593ff9fca4118b4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86b2c9d8eba322dee912",
+    "url": "/static/css/main.0994e523.chunk.css"
+  },
+  {
+    "revision": "d19f16c67a8276a1bf3a",
+    "url": "/static/js/2.16fadca9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16fadca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b2c9d8eba322dee912",
+    "url": "/static/js/main.fdff02df.chunk.js"
+  },
+  {
+    "revision": "9edd54fbd4aacd486b33",
+    "url": "/static/js/runtime-main.c55a3785.js"
+  }
+]);
